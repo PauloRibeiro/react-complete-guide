9e830961a0bb2714168746ce76ab10cad28033e4
GRUPOAMIL\pribeiro05 <pribeiro05@prestadores.amil.com.br>
2018-10-02 16:19:32 -0300

    Finish section 3

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,8 +1,10 @@
 import  React from 'react';
 
+import './Person.css';
+
 const person  = (props) => {
 	return (
-		<div>
+		<div className="Person">
 			<p onClick={props.click}>Hi, I'm a {props.name} and I am {props.age} years old!</p>
 			<span>{props.children}</span>
 			<input type="text" onChange={props.changed} value={props.name} />
